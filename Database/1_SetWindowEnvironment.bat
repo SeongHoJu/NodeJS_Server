f4991512183c4017f9c507522d8d4db65784ea2e
SeongHoJu <hakgu87@gmail.com>
2015-12-01 23:23:15 +0900

    [PROG_FIX] Setting Bat files modify

--- Database/1_SetWindowEnvironment.bat
+++ Database/1_SetWindowEnvironment.bat
@@ -2,8 +2,10 @@
 
 set CURRENT_DIR=%~dp0
 set MONGO_PATH=%CURRENT_DIR%
+set SYS_PATH=%path%
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v MONGO_HOME /d %CURRENT_DIR%
-reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /d "%path%%MONGO_PATH%bin;" /f
+
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v MONGO_HOME /d %CURRENT_DIR% 
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /d "%SYS_PATH%;%%MONGO_HOME%%bin;" /f
 
 pause
